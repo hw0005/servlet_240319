439160b0f9d4069ab5815c722a6af04772761150
DESKTOP-BNIS1T3\user <test@test.com>
2024-06-07 18:49:41 +0900

    servlet_240607

---
+++ src/main/java/com/test/lesson03/DeleteQuiz03.java
@@ -0,0 +1,39 @@
+package com.test.lesson03;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/lesson03/quiz03_delete")
+public class DeleteQuiz03 extends HttpServlet{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// request params
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		// db 연결
+		MysqlService ms = MysqlService.getInstance();
+		ms.connect();
+		
+		// delete query 수행
+		String delQuery = "delete from `bookmark` where `id` =" + id;
+		
+		try {
+			ms.update(delQuery);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// db 해제
+		ms.disconnect();
+		
+		// 목록에 redirect
+		response.sendRedirect("/lesson03/quiz03.jsp");
+	}
+	
+}
---
+++ src/main/java/com/test/lesson03/InsertQuiz03.java
@@ -0,0 +1,44 @@
+package com.test.lesson03;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.test.common.MysqlService;
+
+@WebServlet("/lesson03/quiz03_insert")
+public class InsertQuiz03 extends HttpServlet{
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		// request params
+		String name = request.getParameter("name");
+		String url = request.getParameter("url");
+		
+		// db 연결
+		MysqlService ms = MysqlService.getInstance();
+		ms.connect();
+		
+		// insert query
+		String insertQuery = "insert into `bookmark`"
+				+ "(`name`, `url`)"
+				+ "values"
+				+ "('" + name + "', '" + url + "')";
+		
+		try {
+			ms.update(insertQuery);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// db 연결 해제
+		ms.disconnect();
+		
+		// 목록 화면에 redirect
+		response.sendRedirect("/lesson03/quiz03.jsp");
+	}
+}
